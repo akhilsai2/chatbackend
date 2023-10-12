3acf628647cd3d2b8235cc932a9ec1ad1e9f28aa
Akhil <saiakil456@gmail.com>
2023-10-12 16:48:05 +0530

    changes

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => {
         console.log("User Disconnected", socket.handshake.auth.username);
     });
-
+    console.log(userConnection)
 
 
 });
