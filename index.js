7a4084918e8a580bce7500e6a941fd6b247c6641
Akhil <saiakil456@gmail.com>
2023-10-12 15:01:35 +0530

    server starts

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express")
+const cors = require("cors")
+const http = require("http");
+const app = express()
+const { Server } = require("socket.io")
+app.use(cors({
+    origin: "*",
+    credentials: true
+}))
+app.use(express.json())
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+}
+);
+const userConnection = []
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.handshake.auth.username}`);
+    socket.join(socket.id)
+
+    userConnection.push({ userId: socket.id, userName: socket.handshake.auth.username })
+    console.log(userConnection)
+    socket.on("message", ({ content, sender }) => {
+        const Receiver = userConnection.filter((x) => x.userName !== sender)[0].userId
+        const senderUser = userConnection.filter((x) => x.userName === sender)[0].userId
+        socket.to(Receiver).to(Receiver).emit("message", {
+            content,
+            from: senderUser,
+            to: Receiver,
+        });
+        socket.emit("chatMessage", { content })
+    });
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.handshake.auth.username);
+    });
+
+
+
+});
+// io.use((socket, next) => {
+//     const username = socket.handshake.auth.username;
+//     if (!username) {
+//         return next(new Error("invalid username"))
+//     }
+//     socket.username = username;
+//     next();
+// })
+
+server.listen(8800, () => {
+    console.log("Server Connected 8800 ")
+
+})
