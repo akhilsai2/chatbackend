30d650e691315cc27040556c0b39b42daa780b29
Akhil <saiakil456@gmail.com>
2023-10-13 16:40:56 +0530

    changes in url

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "https://6527e16b4e65c804bea4ba83--luxury-syrniki-6547b4.netlify.app",
+        origin: "https://65291bcc1d133d1cd259f772--scintillating-crisp-25e6ce.netlify.app/",
         // origin: "http://localhost:3000",
         methods: ["GET", "POST"],
         credentials: true
