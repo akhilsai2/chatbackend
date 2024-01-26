3c410ee5ed6742057e80ff6657fd706f9e009d37
Akhil <saiakil456@gmail.com>
2024-01-26 14:13:46 +0530

    add limit

--- index.js
+++ index.js
@@ -35,8 +35,7 @@ app.use(cors({
     methods: ["GET", "POST"],
     credentials: true
 }))
-app.use(express.json())
-app.use(express.limit(100000))
+app.use(express.json({ limit: "50mb" }))
 
 app.post("/generateImg", async function (req, res) {
     async function getGenerated() {
